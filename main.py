b65be61f01664d0e7e5bad9a6efc9f3df40538de
Cristiano Lima Oliveira <59293960+CristianoLimaOliveira@users.noreply.github.com>
2021-07-23 13:55:13 -0300

    Gráfico L2 pronto

---
+++ main.py
@@ -0,0 +1,164 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+def graficoL2(dicionarioAthlete,dicionarioComiteOlimpico):
+	paisEscolhido=1
+	paises=[]
+	while(paisEscolhido!=0):
+		print("Escolha abaixo um país:")
+		contador=0
+		for chave, valor in dicionarioComiteOlimpico.items():
+			print("	"+str(contador+1)+" - "+valor["region"])
+			paises.append(chave)
+			contador=contador+1
+		print("	0 - Voltar")
+		print("Resposta: ", end="")
+		paisEscolhido=int(input())
+		if(paisEscolhido>0 and paisEscolhido<=len(paises)):
+			x=[]
+			for key, value in dicionarioAthlete.items():
+				if(value['noc']==paises[paisEscolhido-1]):
+					if value['year'] not in x:
+						x.append(value['year'])
+			x.sort()
+
+			opcao=3
+			while(opcao!=1 and opcao!=2 and opcao!=0):
+				print ("\n" * 130)
+				print("Escolha a temporada:")
+				print("	1 - Verão")
+				print("	2 - Inverno")
+				print("	0 - Voltar")
+				print("Resposta: ", end="")
+				opcao=int(input())
+				print ("\n" * 130)
+
+			homemArray=[]
+			mulherArray=[]
+			ambosArray=[]
+			if(opcao==1):
+				for i in x:
+					homem=0
+					mulher=0
+					ambos=0
+					for key, value in dicionarioAthlete.items():
+						if(value['noc']==paises[paisEscolhido-1]):
+							if(value['sex']=='M' and value['season']=='Summer' and value['year']==i):
+								homem=homem+1
+							elif(value['sex']=='F' and value['season']=='Summer' and value['year']==i):
+								mulher=mulher+1
+					homemArray.append(homem)
+					mulherArray.append(mulher)
+					ambos=homem+mulher
+					ambosArray.append(ambos)
+			elif(opcao==2):
+				for i in x:
+					homem=0
+					mulher=0
+					ambos=0
+					for key, value in dicionarioAthlete.items():
+						if(value['noc']==paises[paisEscolhido-1]):
+							if(value['sex']=='M' and value['season']=='Winter' and value['year']==i):
+								homem=homem+1
+							elif(value['sex']=='F' and value['season']=='Winter' and value['year']==i):
+								mulher=mulher+1
+					homemArray.append(homem)
+					mulherArray.append(mulher)
+					ambos=homem+mulher
+					ambosArray.append(ambos)
+			x1 = np.array(x)
+			y1 = np.array(homemArray)
+			y2 = np.array(mulherArray)
+			y3 = np.array(ambosArray)
+			plt.plot(x1, y1, "-r", label="Mulheres")
+			plt.plot(x1, y2, "-b", label="Homens")
+			plt.plot(x1, y3, "-y", label="Ambos")
+			plt.legend(loc="upper left")
+			plt.plot(y1, marker = 'o')
+			plt.plot(y2, marker = 'o')
+			plt.plot(y3, marker = 'o')
+			plt.grid(axis = 'y')
+			plt.grid(axis = 'x')
+			plt.xlabel("Anos de olimpíadas do país")
+			plt.ylabel("Quantidade de atletas")
+			plt.title(paises[paisEscolhido-1])
+			plt.show()
+		elif(paisEscolhido==0):
+			return
+		paises.clear()
+		print ("\n" * 130)
+
+def carregandoDados(dicionarioAthlete,dicionarioComiteOlimpico,dicionarioContinentes):
+	with open("athlete_events.xls","r") as csvarquivo:
+		arquivo=csv.reader(csvarquivo)
+		next(arquivo)
+		for linha in arquivo:
+			dicionarioAthlete[linha[0]]={
+				"name":linha[1],
+				"sex":linha[2],
+				"age":linha[3],
+				"height":linha[4],
+				"weight":linha[5],
+				"team":linha[6],
+				"noc":linha[7],
+				"games":linha[8],
+				"year":linha[9],
+				"season":linha[10],
+				"city":linha[11],
+				"sport":linha[12],
+				"event":linha[13],
+				"medal":linha[14],
+			}
+	csvarquivo.close()
+
+	with open("noc_regions.csv", "r") as csvarquivo:
+		arquivo=csv.reader(csvarquivo)
+		next(arquivo)
+		for linha in arquivo:
+			dicionarioComiteOlimpico[linha[0]]={
+				"region":linha[1],
+				"notes":linha[2],
+			}
+	csvarquivo.close()
+	
+	with open("countries-continents.csv", "r") as csvarquivo:
+		arquivo=csv.reader(csvarquivo)
+		next(arquivo)
+		for linha in arquivo:
+			dicionarioContinentes[linha[1]]={
+				"continente":linha[0],
+			}
+	csvarquivo.close()
+
+def main():
+	opcao=1
+	dicionarioAthlete={}
+	dicionarioComiteOlimpico={}
+	dicionarioContinentes={}
+	carregandoDados(dicionarioAthlete,dicionarioComiteOlimpico,dicionarioContinentes)
+	while(opcao!=0):
+		print ("\n" * 130)
+		print("Escolha abaixo qual gráfico deseja exibir:")
+		print("	1 - Gráfico de linhas L2")
+		print("	2 - Gráfico de barras B3")
+		print("	3 - Gráfico de boxplot X3")
+		print("	4 - Resposta textual T7")
+		print("	0 - Sair")
+		print("Resposta: ", end="")
+		opcao=int(input())
+		print ("\n" * 130)
+		if(opcao==1):
+			graficoL2(dicionarioAthlete,dicionarioComiteOlimpico)
+		elif(opcao==2):
+			#graficoB3()
+			print("em breve B3")
+		elif(opcao==3):
+			#graficoX3()
+			print("em breve X3")
+		elif(opcao==4):
+			#respostaTextualT7()
+			print("em breve T7")
+		elif(opcao==0):
+			return
+main()
